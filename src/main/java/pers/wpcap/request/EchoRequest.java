1a01fbc92599a5ec256eeacf011c0f20c111147f
Haisong <haisong@pinduoduo.com>
2017-06-14 17:54:45 +0800

    @JsonProperty tester

--- src/main/java/pers/wpcap/request/EchoRequest.java
+++ src/main/java/pers/wpcap/request/EchoRequest.java
@@ -8,18 +8,24 @@ import java.io.Serializable;
  * Created with IntelliJ IDEA on 2017/6/14.
  * ClassName: EchoRequest
  * Created by haisong
- * Description:
+ * Description: echo tester
+ * https://stackoverflow.com/questions/8560348/different-names-of-json-property-during-serialization-and-deserialization
  */
 public class EchoRequest implements Serializable {
 
-    @JsonProperty("camel_case")
     private Integer camelCase;
 
     public Integer getCamelCase() {
         return camelCase;
     }
 
+    @JsonProperty("camelCase")
     public void setCamelCase(Integer camelCase) {
+        this.camelCase = camelCase;
+    }
+
+    @JsonProperty("camel_case")
+    public void setSnakeCamelCase(Integer camelCase) {
         this.camelCase = camelCase;
     }
 }
